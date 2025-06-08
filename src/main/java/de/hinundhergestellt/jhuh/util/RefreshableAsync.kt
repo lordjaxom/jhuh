1a71fafc26fd509569579991c4a34e85229fb08d
Sascha Volkenandt <sascha@akv-soft.de>
2025-06-08 12:32:01 +0200

    Improve RefreshableAsync using Either and protect future

--- src/main/java/de/hinundhergestellt/jhuh/util/RefreshableAsync.kt
+++ src/main/java/de/hinundhergestellt/jhuh/util/RefreshableAsync.kt
@@ -1,5 +1,9 @@
 package de.hinundhergestellt.jhuh.util
 
+import arrow.atomic.Atomic
+import arrow.atomic.update
+import arrow.core.Either
+import arrow.core.getOrElse
 import org.springframework.core.task.AsyncTaskExecutor
 import java.util.Collections.synchronizedList
 import kotlin.reflect.KProperty
@@ -10,32 +14,31 @@ class RefreshableAsync<T>(
     private val executor: AsyncTaskExecutor,
     private val initializer: () -> T
 ) {
-    private var errorOrValue by atomic(Pair<Throwable?, T?>(null, null))
-    private var future by atomic(executor.submit { update() })
+    private var errorOrValue by atomicNullable<Either<Throwable, T>>()
+    private val future = Atomic(executor.submit { update() })
 
     val stateChangeListeners: MutableList<() -> Unit> = synchronizedList(mutableListOf())
 
     operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
-        errorOrValue.run {
-            first?.let { throw it } // if there's a stored error, throw it, otherwise...
-            second?.let { return it } // ...if there's a stored value, return in, otherwise...
-        }
-        future.get() // ...there's a future that will set valueOrError when finished, so...
-        errorOrValue.run {
-            first?.let { throw it } // ...if there's a stored error, throw it, otherwise...
-            return second!! // there's a value
-        }
+        errorOrValue
+            ?.getOrElse { throw it } // if there's a stored error, throw it, otherwise...
+            ?.let { return it } // ...if there's a stored value, return it, otherwise...
+        future.get().get() // ...there's a future that will set valueOrError when finished, so...
+        return errorOrValue!!.getOrElse { throw it }  // ...if there's a stored error, throw it, otherwise there's a value to return
     }
 
     fun refresh() {
-        future = executor.submit { update() }
+        future.update { if (it.isDone) executor.submit { update() } else it }
     }
 
     private fun update() {
         errorOrValue = try {
-            Pair(null, initializer())
+            Either.Right(initializer())
         } catch (e: Throwable) {
-            Pair(e, null)
+            Either.Left(e)
+        }
+        synchronized(stateChangeListeners) {
+            stateChangeListeners.forEach { it() }
         }
     }
 }
