3dd126ea8baaceff7f4bdeb017e470334d535125
Sascha Volkenandt <sascha@akv-soft.de>
2025-07-07 12:38:31 +0200

    Rename Rayher Silikonformen

--- hinundhergestellt-tools/src/test/kotlin/de/hinundhergestellt/jhuh/RayherProductsFixITCase.kt
+++ hinundhergestellt-tools/src/test/kotlin/de/hinundhergestellt/jhuh/RayherProductsFixITCase.kt
@@ -1,5 +1,6 @@
 package de.hinundhergestellt.jhuh
 
+import de.hinundhergestellt.jhuh.core.ifDirty
 import de.hinundhergestellt.jhuh.vendors.rayher.csv.readRayherProducts
 import de.hinundhergestellt.jhuh.vendors.ready2order.client.ArtooProductClient
 import de.hinundhergestellt.jhuh.vendors.ready2order.client.ArtooProductGroupClient
@@ -8,6 +9,7 @@ import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.reactive.collect
 import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Disabled
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.io.ByteArrayResource
@@ -22,7 +24,10 @@ import kotlin.test.Test
 
 private val logger = KotlinLogging.logger {}
 
+private val downloadsDirectory = Path(System.getProperty("user.home")).resolve("Downloads")
+
 @SpringBootTest
+@Disabled("Only run manually")
 class RayherProductsFixITCase {
 
     @Autowired
@@ -37,21 +42,37 @@ class RayherProductsFixITCase {
 
     @Test
     fun fixRayherSilikonformen() = runBlocking {
-        val rayherProducts = readRayherProducts(Path("/home/lordjaxom/Downloads/Rayher0725d_Excel.csv"))
+        val rayherProducts = readRayherProducts(downloadsDirectory.resolve("Rayher0725d_Excel.csv"))
 
         val productGroup = artooProductGroupClient.findAll().first { it.name == "Rayher Silikonformen" }
         artooProductClient.findAll()
             .filter { it.productGroupId == productGroup.id }
             .collect { product ->
-                val itemNumber = product.itemNumber?.run {
-                    if (contains("-")) replace("-", "").also { product.itemNumber = it }
-                    else this
-                }!!
+                val itemNumber = fixItemNumber(product.itemNumber!!).also { product.itemNumber = it }
                 val rayherProduct = rayherProducts.firstOrNull { it.articleNumber == itemNumber }!!
+                product.name = rayherProduct.descriptions[0]
+                product.description = rayherProduct.description
+                    .replace(", SB-Box 1Stück", "")
+                    .replace(", Box 1Stück", "")
 
-                logger.info { "${product.name} -> ${rayherProduct.descriptions[0]}" }
-                logger.info { "    ${rayherProduct.description}" }
-                logger.info { "    ${rayherProduct.imageUrls}" }
+                product.ifDirty {
+                    logger.info { "${it.name} (${it.itemNumber})" }
+                    logger.info { "    ${it.description}" }
+                    artooProductClient.update(it)
+                }
+            }
+    }
+
+    @Test
+    fun downloadProductImages() = runBlocking {
+        val rayherProducts = readRayherProducts(downloadsDirectory.resolve("Rayher0725d_Excel.csv"))
+
+        val productGroup = artooProductGroupClient.findAll().first { it.name == "Rayher Silikonformen" }
+        artooProductClient.findAll()
+            .filter { it.productGroupId == productGroup.id }
+            .collect { product ->
+                val itemNumber = fixItemNumber(product.itemNumber!!)
+                val rayherProduct = rayherProducts.firstOrNull { it.articleNumber == itemNumber }!!
 
                 val imageDirName = "Rayher ${rayherProduct.descriptions[0]}"
                 val imagePath = Path("/home/lordjaxom/Dokumente/Hin-undHergestellt/Shopify").resolve(imageDirName)
@@ -67,4 +88,8 @@ class RayherProductsFixITCase {
                 }
             }
     }
+
+    private fun fixItemNumber(itemNumber: String) =
+        if (itemNumber.contains("-")) itemNumber.replace("-", "")
+        else itemNumber
 }
