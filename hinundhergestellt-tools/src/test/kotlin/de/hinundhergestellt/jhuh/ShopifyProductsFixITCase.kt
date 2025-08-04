9eaa1e76c38dbcf2f9e8d7dcca6ceef42a848a3f
Sascha Volkenandt <sascha@akv-soft.de>
2025-08-04 08:11:29 +0200

    Implement more tools for Shopify

--- hinundhergestellt-tools/src/test/kotlin/de/hinundhergestellt/jhuh/ShopifyProductsFixITCase.kt
+++ hinundhergestellt-tools/src/test/kotlin/de/hinundhergestellt/jhuh/ShopifyProductsFixITCase.kt
@@ -3,31 +3,37 @@ package de.hinundhergestellt.jhuh
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import de.hinundhergestellt.jhuh.vendors.shopify.client.LinkedMetafield
+import de.hinundhergestellt.jhuh.vendors.shopify.client.ProductOptionValue
 import de.hinundhergestellt.jhuh.vendors.shopify.client.ShopifyMedia
 import de.hinundhergestellt.jhuh.vendors.shopify.client.ShopifyMediaClient
-import de.hinundhergestellt.jhuh.vendors.shopify.client.ShopifyMetafieldsClient
+import de.hinundhergestellt.jhuh.vendors.shopify.client.ShopifyMetafieldClient
+import de.hinundhergestellt.jhuh.vendors.shopify.client.ShopifyMetaobjectClient
 import de.hinundhergestellt.jhuh.vendors.shopify.client.ShopifyProductClient
 import de.hinundhergestellt.jhuh.vendors.shopify.client.ShopifyProductOptionClient
+import de.hinundhergestellt.jhuh.vendors.shopify.client.ShopifyProductVariant
 import de.hinundhergestellt.jhuh.vendors.shopify.client.ShopifyProductVariantClient
 import de.hinundhergestellt.jhuh.vendors.shopify.client.ShopifyWeight
+import de.hinundhergestellt.jhuh.vendors.shopify.client.UnsavedShopifyMetaobject
+import de.hinundhergestellt.jhuh.vendors.shopify.client.withLinkedMetafieldValue
+import de.hinundhergestellt.jhuh.vendors.shopify.graphql.types.LinkedMetafield
+import de.hinundhergestellt.jhuh.vendors.shopify.graphql.types.MetaobjectField
+import de.hinundhergestellt.jhuh.vendors.shopify.graphql.types.ProductOptionValue
 import de.hinundhergestellt.jhuh.vendors.shopify.graphql.types.WeightUnit
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Disabled
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import java.nio.charset.StandardCharsets
-import kotlin.collections.filter
 import kotlin.io.path.Path
+import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.moveTo
 import kotlin.io.path.readText
 import kotlin.io.path.writeText
-import kotlin.sequences.filter
-import kotlin.sequences.toList
+import kotlin.math.sqrt
 import kotlin.test.Test
 
 @SpringBootTest
@@ -44,10 +50,13 @@ class ShopifyProductsFixITCase {
     private lateinit var variantClient: ShopifyProductVariantClient
 
     @Autowired
-    private lateinit var metafieldsClient: ShopifyMetafieldsClient
+    private lateinit var metafieldsClient: ShopifyMetafieldClient
 
     @Autowired
     private lateinit var mediaClient: ShopifyMediaClient
+
+    @Autowired
+    private lateinit var metaobjectClient: ShopifyMetaobjectClient
 
     @Test
     fun findAllProducts(): Unit = runBlocking {
@@ -55,28 +64,160 @@ class ShopifyProductsFixITCase {
     }
 
     @Test
-    fun associateMedia(): Unit = runBlocking {
+    fun findSingleProduct() = runBlocking {
+        val product = productClient.fetchAll("'POLI-FLEX® PEARL GLITTER*'").toList()
+        println(product)
+    }
+
+    @Test
+    fun findAndRenameImagesBySKU() = runBlocking {
+        val imagePath = Path("/home/lordjaxom/Projects/Java/jhuh/hinundhergestellt-tools/src/test/python")
         val product = productClient.fetchAll().first { it.title.startsWith("craftcut® glänzend") }
-        val changedVariants = product.variants.asSequence()
-            .filter { it.options.isNotEmpty() && it.mediaId == null }
-            .map {
-                it to product.media.filter { image ->
-                    image.src.contains("""\b${Regex.fromLiteral(it.sku)}\b""".toRegex(RegexOption.IGNORE_CASE))
-                }
+        product.variants.forEach { variant ->
+            val image = imagePath.listDirectoryEntries("*-${variant.sku}.jpg").firstOrNull()
+            val newFileName = "craftcut-vinyl-glaenzend-${variant.sku.lowercase()}-${variant.title.replace(" ", "-").lowercase()}.jpg"
+            image?.moveTo(imagePath.resolve(newFileName))
+        }
+    }
+
+    @Test
+    fun generateAltTextsAndAssociateMediaBySKU() = runBlocking {
+        val product = productClient.fetchAll().first { it.title.startsWith("POLI-FLEX® PEARL GLITTER") }
+
+        val variants = mutableListOf<ShopifyProductVariant>()
+        val media = mutableListOf<ShopifyMedia>()
+        product.variants.forEach { variant ->
+            val image = product.media.find { it.src.contains("-${variant.sku.lowercase()}-") }
+            if (image == null) return@forEach
+
+            if (variant.mediaId == null) {
+                variant.mediaId = image.id
+                variants.add(variant)
             }
-            .filter { (_, images) -> images.size == 1 }
-            .onEach { (variant, images) -> variant.mediaId = images[0].id }
-            .map { (variant, _) -> variant }
+            if (image.altText.isEmpty()) {
+                image.altText = "${product.title} in Farbe ${variant.options[0].value}"
+                media.add(image)
+            }
+        }
+
+        variantClient.update(product, variants)
+        mediaClient.update(media)
+    }
+
+    @Test
+    fun deleteUnusedFiles() = runBlocking {
+        val excludePrefixes = listOf("startseite-")
+        val unusedFiles = mediaClient.fetchAll("used_in:none")
+            .filter { media -> excludePrefixes.none { media.fileName.startsWith(it) } }
             .toList()
-        variantClient.update(product, changedVariants)
+        println("Deleting ${unusedFiles.size} unused files")
+        //mediaClient.delete(unusedFiles)
+    }
+
+    @Test
+    fun replaceImagesAndAssociate() = runBlocking {
+        val imagePath = Path("/home/lordjaxom/Dokumente/Hin-undHergestellt/Shopify TEMP/POLI-TAPE TUBITHERM")
+        val product = productClient.fetchAll("'POLI-TAPE® TUBITHERM®*'").first()
+
+        // save variants' media before attaching the new ones
+        val mediaToDetach = product.media.filter { media -> product.variants.any { it.mediaId == media.id } }
+
+        val imagesWithVariant = imagePath
+            .listDirectoryEntries("poli-tape-tubitherm-*.png")
+            .asSequence()
+            .map { image -> image to product.variants.first { image.fileName.toString().contains("-${it.sku.substring(4, 7)}-") } }
+            .toList()
+
+        val mediasWithVariant = mediaClient
+            .upload(imagesWithVariant.map { it.first })
+            .asSequence()
+            .zip(imagesWithVariant.asSequence().map { it.second })
+            .onEach { (media, variant) ->
+                media.altText = "${product.title} in Farbe ${variant.options[0].value}"
+                variant.mediaId = media.id
+            }
+            .toList()
+
+        mediaClient.update(mediasWithVariant.map { it.first }, referencesToAdd = listOf(product.id))
+        variantClient.update(product, mediasWithVariant.map { it.second })
+        mediaClient.update(mediaToDetach, referencesToRemove = listOf(product.id))
+    }
+
+    @Test
+    fun createColorSwatchesAndAssociate() = runBlocking {
+        val definition = metaobjectClient.fetchDefinitionByType("shopify--color-pattern")!!
+        val taxonomyReferences = definition.metaobjects.asSequence()
+            .map { metaobject -> metaobject.fields.find { it.key == "color" }!!.value!! to metaobject.fields.find { it.key == "color_taxonomy_reference" }!!.value!! }
+            .toList()
+
+        fun hexToRgb(hex: String): Triple<Int, Int, Int> {
+            val cleanHex = hex.removePrefix("#")
+            val r = cleanHex.substring(0, 2).toInt(16)
+            val g = cleanHex.substring(2, 4).toInt(16)
+            val b = cleanHex.substring(4, 6).toInt(16)
+            return Triple(r, g, b)
+        }
+
+        fun colorDistance(a: Triple<Int, Int, Int>, b: Triple<Int, Int, Int>): Double {
+            val dr = a.first - b.first
+            val dg = a.second - b.second
+            val db = a.third - b.third
+            return sqrt((dr * dr + dg * dg + db * db).toDouble())
+        }
+
+        fun findClosestTaxonomy(inputHex: String): String {
+            val targetRgb = hexToRgb(inputHex)
+            return taxonomyReferences.minByOrNull { colorDistance(hexToRgb(it.first), targetRgb) }!!.second
+        }
+
+        val imagePath = Path("/home/lordjaxom/Dokumente/Hin-undHergestellt/Shopify TEMP/POLI-TAPE TUBITHERM")
+        val product = productClient.fetchAll("'POLI-TAPE® TUBITHERM®*'").first()
+
+        val optionValuesToUpdate = mutableListOf<ProductOptionValue>()
+        imagePath
+            .listDirectoryEntries("poli-tape-tubitherm-*.png")
+            .map { image -> image to product.variants.first { image.fileName.toString().contains("-${it.sku.substring(4, 7)}-") } }
+            .map { (image, variant) -> Triple(image, variant, product.options[0].optionValues.first { it.name == variant.options[0].value }) }
+            .forEach { (image, variant, optionValue) ->
+                val process = Runtime.getRuntime().exec(
+                    arrayOf(
+                        "python",
+                        "/home/lordjaxom/Projects/Java/jhuh/hinundhergestellt-tools/src/test/python/dominant_color.py",
+                        image.toString(),
+                        "100"
+                    )
+                )
+                val color = process.inputStream.bufferedReader(Charsets.UTF_8).use { it.readText().trim() }
+
+                val handle = "poli-tubitherm-${variant.options[0].value.lowercase().replace(" ", "-")}"
+                val taxonomyReference = findClosestTaxonomy(color)
+                val unsaved = UnsavedShopifyMetaobject(
+                    "shopify--color-pattern",
+                    handle,
+                    listOf(
+                        MetaobjectField.Builder().withKey("label").withValue(variant.options[0].value).build(),
+                        MetaobjectField.Builder().withKey("color").withValue(color).build(),
+                        MetaobjectField.Builder().withKey("color_taxonomy_reference").withValue(taxonomyReference).build(),
+                        MetaobjectField.Builder().withKey("pattern_taxonomy_reference").withValue("gid://shopify/TaxonomyValue/2874").build()
+                    )
+                )
+                val metaobject = metaobjectClient.create(unsaved)
+
+                optionValuesToUpdate.add(optionValue.withLinkedMetafieldValue(metaobject.id))
+            }
+
+        product.options[0].linkedMetafield = LinkedMetafield("shopify", "color-pattern")
+        optionClient.update(product, product.options[0])
     }
 
     @Test
     fun findProductsWithoutWeight() = runBlocking {
         val products = productClient.fetchAll().toList()
         products.forEach { product ->
-            if (product.variants.any { it.weight.value == 0.0 }) {
-                println(product.title)
+            product.variants.forEach { variant ->
+                if (variant.weight.value == 0.0) {
+                    println("${product.title} - ${variant.title}")
+                }
             }
         }
     }
@@ -84,11 +225,20 @@ class ShopifyProductsFixITCase {
     @Test
     fun assignWeightToProducts() = runBlocking {
         productClient.fetchAll()
-            .filter { it.title.contains("Siser® Aurora") }
+            .filter { it.title.startsWith("Ricorumi Nilli Nilli") }
             .collect { product ->
-                product.variants.forEach { it.weight = ShopifyWeight(WeightUnit.GRAMS, 0.5) }
+                product.variants.forEach { it.weight = ShopifyWeight(WeightUnit.GRAMS, 25.0) }
                 variantClient.update(product, product.variants)
             }
+    }
+
+    @Test
+    fun updateColorMetaobjects() = runBlocking {
+        val definition = metaobjectClient.fetchDefinitionByType("shopify--color-pattern")!!
+        definition.metaobjects
+            .filter { !it.handle.startsWith("pearl-glitter") }
+            .onEach { it.handle = "craftcut-glossy-${it.handle}" }
+            .forEach { metaobjectClient.update(it) }
     }
 
     @Test
@@ -101,8 +251,7 @@ class ShopifyProductsFixITCase {
             homeDirectory
                 .resolve("Dokumente/products.json")
                 .writeText(objectMapper.writeValueAsString(products), StandardCharsets.UTF_8)
-        }
-        else println("No media without alt texts found")
+        } else println("No media without alt texts found")
     }
 
     @Test
